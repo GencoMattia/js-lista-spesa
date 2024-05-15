8bfdfa4e69a46ca9cb1aee5542367237a5b4862a
GencoMattia <gencomattia@hotmail.it>
2024-05-15 14:39:33 +0200

    added limiter

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 const shoppingList = ["Parrucca", "Gelati", "Mozzarelle", "Lanciafiamme", "Spada Vorpal", "Set dadi"];
 let articleEl = document.createElement("li")
 let listEl = document.querySelector("ul")
+
+const listArticles = shoppingList.length
